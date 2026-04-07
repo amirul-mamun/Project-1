4df4def1b43da426b33cf6db7a4f9b708b79ab42
amirul-mamun <amirulislam21354@gmail.com>
2026-04-07 10:17:16 +0600

    update

--- project1/script.js
+++ project1/script.js
@@ -1,18 +1,41 @@
-let div = document.querySelectorAll('.elem')
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
 
-div.forEach(function(val){
-    val.addEventListener('mouseenter',function(){
-        val.childNodes[3].style.opacity = 1;
-    })
-    val.addEventListener('mouseleave',function(){
-        val.childNodes[3].style.opacity = 0;
-    })
-    val.addEventListener('mousemove', function(dets){
-        let rect = val.getBoundingClientRect()
-        
-       val.childNodes[3].style.top =( dets.clientY - rect.top)+ 'px';
-       val.childNodes[3].style.left = ( dets.clientX - rect.left)+ 'px';
-      
+function circleMouse(){
+  window.addEventListener('mousemove', function(dets){
+    this.document.querySelector('.minicircle').style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
+  })
+}
 
+circleMouse()
+
+
+
+function firstPageAnim() {
+  var tl = gsap.timeline();
+
+  tl.from(".nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".smoothElem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: -1,
+      stagger: 0.2,
     })
-})
+    .from(".herofoter", {
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
+
+firstPageAnim()
