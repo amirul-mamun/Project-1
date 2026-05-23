c685b449a87d1d069f4f8242974cb93c4a253d72
Amirul Islam Mamun <amirulislam21354@gmail.com>
2026-05-23 10:47:33 +0600

    Delete Custom cursor directory

--- Custom cursor/script.js
+++
@@ -1,8 +0,0 @@
-let mouse = document.querySelector('.mouse')
-let main = document.querySelector('.main')
-
-main.addEventListener('mousemove', (dets) =>{
-   mouse.style.left = dets.x + 'px'
-   mouse.style.top = dets.y + 'px'
-   
-})
